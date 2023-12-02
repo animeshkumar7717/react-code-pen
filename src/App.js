c4cdeeb218402329d67947f0be50720a965b3980
Animesh Kumar <animesh.kumar7717@gmail.com>
2023-12-02 14:01:34 +0530

    develop: code-pen for html, css and js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import img from './image.jpg'
+import useLocalStorage from './storage';
 
-function App() {
+const App = () => {
+  const [html, setHtml] = useLocalStorage('html','');
+  const [css, setCss] = useLocalStorage('css','');
+  const [js, setJs] = useLocalStorage('js','');
+  const [codepenCode, setCodepenCode] = useState('')
+
+  useEffect(()=> {
+    const timerout = setTimeout(()=>{
+      setCodepenCode(`
+      <body>${html}</body>
+      <style>${css}</style>
+      <script>${js}</script>
+      `)
+    }, 200)
+    return () => clearInterval(timerout)
+  }, [html,css,js])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div className='header'>
+        <img src={img} alt='' />
+        <span>Codepen</span>
+      </div>
+      <div className='input-cover'>
+        <textarea className='input' type='text' placeholder='HTMl' value={html} onChange={(e)=>setHtml(e.target.value)} />
+        <div className='width' />
+        <textarea className='input' type='text' placeholder='CSS' value={css} onChange={(e)=>setCss(e.target.value)} />
+        <div className='width' />
+        <textarea className='input' type='text' placeholder='JS' value={js} onChange={(e)=>setJs(e.target.value)} />
+      </div>
+      <div className='output'>
+        <iframe
+        srcDoc={codepenCode}
+        title='output'
+        sandbox='allow-scripts'
+        width='100%'
+        height='100%'
+        />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
